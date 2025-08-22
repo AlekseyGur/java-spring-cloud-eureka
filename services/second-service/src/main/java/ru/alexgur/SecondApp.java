3014feec611e85acb3df22a830faa2b40f320327
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-08-22 19:14:46 +0300

    Eureka

---
+++ services/second-service/src/main/java/ru/alexgur/SecondApp.java
@@ -0,0 +1,13 @@
+package ru.alexgur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondApp.class, args);
+    }
+
+}
