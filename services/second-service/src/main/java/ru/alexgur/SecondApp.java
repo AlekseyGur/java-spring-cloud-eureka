e9a392eabe15a02b4f83e600c3dab4d8089ac876
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-08-27 10:12:25 +0300

    Edit configs

--- services/second-service/src/main/java/ru/alexgur/SecondApp.java
+++ services/second-service/src/main/java/ru/alexgur/SecondApp.java
@@ -2,8 +2,10 @@ package ru.alexgur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SecondApp {
 
     public static void main(String[] args) {
